18cb1b21c3e21729cb3f188132814e50fe0a9805
anhvinhphan659 <anhvinhphan659@gmail.com>
2021-12-05 16:01:07 +0700

    update account manager

--- src/app/controllers/bookController.js
+++ src/app/controllers/bookController.js
@@ -1,52 +1,54 @@
 const adminservice = require('../services/adminService');
 const {
-    multipleSequelizeToObject, SequelizeToObject
+    multipleSequelizeToObject,
+    SequelizeToObject
 } = require('../../util/sequelize')
 
-class bookController { 
+class bookController {
 
     //[PUT] : /books/save/:id
     async saveUpdate(req, res, next) {
-        try {
-            if (!req.user){
-                res.redirect('/login')
-            }
-            const book = await adminservice.getOnebook(req.params.id)
-            book.set(req.body)
-            await book.save();
-            res.redirect('/books/book-manager')
-        }catch (e) {
-            next(e);
-        }
-    }
-    //[GET]: books/:id/edit  
-    async edit(req, res, next){
-        try {
-            if (!req.user){
-                res.redirect('/login')
-            }
-            const book = await adminservice.getOnebook(req.params.id)
-            const NXB = await adminservice.AllNXB()
-            res.render('book/edit',{
-                NXB: multipleSequelizeToObject(NXB),
-                sach : SequelizeToObject(book),
-            })
-        }catch(e){
-            next(e);
-        }
+            try {
+                if (!req.user) {
+                    res.redirect('/login')
+                }
+                const book = await adminservice.getOnebook(req.params.id);
 
-    }
-    //[GET] : /books/input-new-book
+                book.set(req.body)
+                await book.save();
+                res.redirect('/books/book-manager')
+            } catch (e) {
+                next(e);
+            }
+        }
+        //[GET]: books/:id/edit  
+    async edit(req, res, next) {
+            try {
+                if (!req.user) {
+                    res.redirect('/login')
+                }
+                const book = await adminservice.getOnebook(req.params.id)
+                const NXB = await adminservice.AllNXB()
+                res.render('book/edit', {
+                    NXB: multipleSequelizeToObject(NXB),
+                    sach: SequelizeToObject(book),
+                })
+            } catch (e) {
+                next(e);
+            }
+
+        }
+        //[GET] : /books/input-new-book
     async inputbook(req, res, next) {
         try {
-            if (!req.user){
+            if (!req.user) {
                 res.redirect('/login')
             }
             const cate = await adminservice.getmodels().theloai.findAll();
             const NXB = await adminservice.AllNXB()
             res.render('book/newbook', {
                 NXB: multipleSequelizeToObject(NXB),
-                Theloai : multipleSequelizeToObject(cate),
+                Theloai: multipleSequelizeToObject(cate),
             });
         } catch (e) {
             next(e)
@@ -59,7 +61,7 @@ class bookController {
     async show(req, res, next) {
         const title = req.query.title;
         try {
-            if (!req.user){
+            if (!req.user) {
                 res.redirect('/login')
             }
             const books = await adminservice.getBooks(title);
@@ -73,45 +75,45 @@ class bookController {
 
     //[POST] : /books/book-manager
     async addBook(req, res, next) {
-        try{
-            if (!req.user){
-                res.redirect('/login')
-            }
-            req.body.masach = await adminservice.genKeybook(req.body.hinhthuc);
-            // insert book to db
-            const book = await adminservice.getmodels().sach.create({
-                masach: req.body.masach,
-                tensach: req.body.tensach,
-                tacgia : req.body.tacgia,
-                MOTA : req.body.MOTA,
-                HINHANH : req.body.HINHANH,
-                manxb : req.body.manxb,
-                ngayXB : req.body.ngayXB,
-                gia : req.body.gia,
-                SL : 0,
-            });
-            req.body.category.forEach(async(element) => {
-                await adminservice.getmodels().theloaicuasach.create({
-                    masach : req.body.masach,
-                    maTL : element,
+            try {
+                if (!req.user) {
+                    res.redirect('/login')
+                }
+                req.body.masach = await adminservice.genKeybook(req.body.hinhthuc);
+                // insert book to db
+                const book = await adminservice.getmodels().sach.create({
+                    masach: req.body.masach,
+                    tensach: req.body.tensach,
+                    tacgia: req.body.tacgia,
+                    MOTA: req.body.MOTA,
+                    HINHANH: req.body.HINHANH,
+                    manxb: req.body.manxb,
+                    ngayXB: req.body.ngayXB,
+                    gia: req.body.gia,
+                    SL: 0,
                 });
-            });
-            //back to create book
-            res.redirect('/books/book-manager')
-        }catch (e) {
-            next(e)
+                req.body.category.forEach(async(element) => {
+                    await adminservice.getmodels().theloaicuasach.create({
+                        masach: req.body.masach,
+                        maTL: element,
+                    });
+                });
+                //back to create book
+                res.redirect('/books/book-manager')
+            } catch (e) {
+                next(e)
+            }
         }
-    }
-    //[DELETE]:  /books/save/:id
-    async saveDelete(req, res, next){
+        //[DELETE]:  /books/save/:id
+    async saveDelete(req, res, next) {
         try {
-            if (!req.user){
+            if (!req.user) {
                 res.redirect('/login')
             }
             console.log('here')
             const book = await adminservice.getOnebook(req.params.id)
             await book.destroy();
-            res.redirect('/books/book-manager') 
+            res.redirect('/books/book-manager')
         } catch (error) {
             next(error);
         }
